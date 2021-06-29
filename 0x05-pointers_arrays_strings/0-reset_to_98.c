8c0ea7524113b179799ea3f61565f6f3da7b7f0c
sakin977 <sakinduku@gmail.com>
2021-06-29 06:08:02 +0300

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,5 +1,12 @@
 #include<stdio.h>
 #include "holberton.h"
+/**
+ * op_add - resets number
+ * @arg1: First operand
+ * @arg2: Second operand
+ *
+ * Return: update
+ */
 
 void reset_to_98(int *n)
 {
