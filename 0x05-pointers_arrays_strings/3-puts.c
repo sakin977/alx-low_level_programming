b547bd5138b8e2b5c4d542c29c7840aeec27ae38
sakin977 <sakinduku@gmail.com>
2021-06-29 18:30:07 +0300

    Create 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+/**
+ * _puts - puts string to stdout
+ *
+ * @str: string to print to stdout
+ *
+ * Return: always void
+ */
+void _puts(char *str)
+{
+	while (*str)
+		_putchar(*str++);
+	_putchar('\n');
+}
