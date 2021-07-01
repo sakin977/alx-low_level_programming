40de2695b571fa0a01320ff12db46312ed38e746
sakin977 <sakinduku@gmail.com>
2021-07-01 16:57:28 +0300

    Create 1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+char *_strcat(char *dest, char *src)
+{
+  
+char *dest[] = "Hello ";
+  
+char *src[] = "World!";
+  
+_strcat (dest, src);
+  
+printf ("Concatenated String: %s\n", destination);
+  
+return 0;
+
+}
