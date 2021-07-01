6adf826c8c6e1fdf565bb4b562e532302c5672a7
sakin977 <sakinduku@gmail.com>
2021-07-01 15:34:32 +0300

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,9 +2,9 @@
 #include "holberton.h"
 int main()
 {
-    char destination[] = "Hello ";
-    char source[] = "World!";
-    strcat(destination,source);
-    printf("Concatenated String: %s\n", destination);
-    return 0;
+char destination[] = "Hello ";
+char source[] = "World!";
+strcat(destination,source);
+printf("Concatenated String: %s\n", destination);
+return 0;
 }
