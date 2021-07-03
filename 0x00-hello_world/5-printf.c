e2657777e7c82a46a5448c16abf0f4ded4a7774e
sakin977 <sakinduku@gmail.com>
2021-07-03 15:48:41 +0300

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,13 @@
-#!/bin/bash
+#include<stdio.h>
+/**
+*main - starting point
+@void: (no parameter)
+*Description: displays the line
+*with proper grammar, but the outcome is a piece of art,
+*Return: Always 0
+*/
+int main(void)
+{
 printf ("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
