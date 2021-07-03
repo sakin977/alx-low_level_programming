c61d34520eb6856aeb7d1f320ee2546f029e758a
sakin977 <sakinduku@gmail.com>
2021-07-03 15:54:41 +0300

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 /**
 *main - starting point
-@void: no parameter
+*@void: no parameter
 *Description: displays the line
 *with proper grammar, but the outcome is a piece of art,
 *Return: Always 0
