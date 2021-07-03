55de56dbf5996f23c64263a6ce6b61ef62d01058
sakin977 <sakinduku@gmail.com>
2021-07-03 15:38:15 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
 /**
-*puts - prints word
+*main - prints out line
+*
+*return-0
 */
-int main()
+int main(void)
 {
-puts ("Programming is like building a multilingual puzzle");
+puts("Programming is like building a multilingual puzzle");
 return 0;
 }
