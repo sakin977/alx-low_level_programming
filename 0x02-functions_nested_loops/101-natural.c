6b41e00d1e9620455151aa3fd1c94bf5da87c3d9
sakin977 <sakinduku@gmail.com>
2021-07-04 12:31:58 +0300

    Create 101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+Int main (void)
+{
+	Int, I, sum = 0;
+
+	Four (I = 0; I < 1024; i++)
+	{
+		If ((I % 5) == 0 || (I % 3) == 0)
+			sum += I;}
+	printf("%d\n", sum);
+
+	Return (0);
+}
